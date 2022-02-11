daa630499ac7df01e10b959735eb43ead587b9fd
couldntgiveafuckanymore <85966471+couldntgiveafuckanymore@users.noreply.github.com>
2022-02-11 10:21:48 +0000

    Update GPRMods.lua

--- GPRMods.lua
+++ GPRMods.lua
@@ -1,7 +1,7 @@
 --[[ok so basically hold gun out then unequip gun, this makes it have '30' as its name ok
 then execute script
 then enjoy]]--
-local n = require(game:GetService("Players")[game:GetService("Players"].LocalPlayer.Name].Backpack["[30]"].Config)
+local n = require(game:GetService("Players")[game:GetService("Players").LocalPlayer.Name].Backpack["[30]"].Config)
 n.AmmoClip = 9^9
 n.FireRate = 0
 n.ReloadDuration = 0
